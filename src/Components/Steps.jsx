bf56e1c7322565f6052cedb01d3c37d3a24a26fc
Himanshu-Webguruz <himanshu@webguruz.in>
2024-06-20 16:06:27 +0530

    updating

--- src/Components/Steps.jsx
+++ src/Components/Steps.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import img1 from "../assets/images/tab-product-icon.png"
-import img2 from "../assets/images/customize-uniform-icon.png"
-import img3 from "../assets/images/tab-product-icon.png"
 export const Steps = ({ gettingdata }) => {
   const [activeState, setActiveState] = useState(1);
 
@@ -15,51 +12,23 @@ export const Steps = ({ gettingdata }) => {
       <ul id="uniform-tabs-nav" className="list-unstyled">
         <li className={`navone ${activeState === 1 ? "active" : ""}`}>
           <a onClick={() => handleStepClick(1)}>
-            {/* <div className="step">
-              <div className="step-no">Step 1</div>
-            </div> */}
+            
             <div className="step-tab">
-              <div className="step-tab-icon">
-                <figure>
-                  <img src={img1} alt="" />
-                </figure>
-              </div>
-              <h3 className="step-tab-name">Select Your Uniform</h3>
               <h3 className="step-tab-name-mob">Select</h3>
             </div>
           </a>
         </li>
         <li className={`navtwo ${activeState === 2 ? "active" : ""}`}>
           <a onClick={() => handleStepClick(2)}>
-            {/* <div className="step">
-              <div className="step-no">Step 2</div>
-            </div> */}
+           
             <div className="step-tab">
-              <div className="step-tab-icon">
-                <figure>
-                  <img
-                    src= {img2}
-                    alt=""
-                  />
-                </figure>
-              </div>
-              <h3 className="step-tab-name">Customize Your Uniform</h3>
               <h3 className="step-tab-name-mob">Customize</h3>
             </div>
           </a>
         </li>
         <li className={`navthree ${activeState === 3 ? "active" : ""}`}>
           <a onClick={() => handleStepClick(3)}>
-            {/* <div className="step">
-              <div className="step-no">Step 3</div>
-            </div> */}
             <div className="step-tab">
-              <div className="step-tab-icon">
-                <figure>
-                  <img src={img3} alt="" />
-                </figure>
-              </div>
-              <h3 className="step-tab-name">Finalize The Uniform</h3>
               <h3 className="step-tab-name-mob">Finalize</h3>
             </div>
           </a>
--- src/Components/UniformStore/PreviewStore.jsx
+++ src/Components/UniformStore/PreviewStore.jsx
@@ -33,58 +33,58 @@ import jerseyPreviews32 from "../../assets/images/uniform-previews/OUD-32.png";
 import jerseyPreviews33 from "../../assets/images/uniform-previews/OUD-33.png";
 import jerseyPreviews34 from "../../assets/images/uniform-previews/OUD-34.png";
 import jerseyPreviews35 from "../../assets/images/uniform-previews/OUD-35.png";
-// import jerseyPreviews36 from "../../assets/images/uniform-previews/OUD-36.png";
-// import jerseyPreviews37 from "../../assets/images/uniform-previews/OUD-37.png";
+import jerseyPreviews36 from "../../assets/images/uniform-previews/OUD-36.png";
+import jerseyPreviews37 from "../../assets/images/uniform-previews/OUD-37.png";
 import jerseyPreviews38 from "../../assets/images/uniform-previews/OUD-38.png";
-// import jerseyPreviews39 from "../../assets/images/uniform-previews/OUD-39.png";
-// import jerseyPreviews40 from "../../assets/images/uniform-previews/OUD-40.png";
-// import jerseyPreviews41 from "../../assets/images/uniform-previews/OUD-41.png";
-// import jerseyPreviews42 from "../../assets/images/uniform-previews/OUD-42.png";
-// import jerseyPreviews43 from "../../assets/images/uniform-previews/OUD-43.png";
-// import jerseyPreviews44 from "../../assets/images/uniform-previews/OUD-44.png";
-// import jerseyPreviews45 from "../../assets/images/uniform-previews/OUD-45.png";
-// import jerseyPreviews46 from "../../assets/images/uniform-previews/OUD-46.png";
+import jerseyPreviews39 from "../../assets/images/uniform-previews/OUD-39.png";
+import jerseyPreviews40 from "../../assets/images/uniform-previews/OUD-40.png";
+import jerseyPreviews41 from "../../assets/images/uniform-previews/OUD-41.png";
+import jerseyPreviews42 from "../../assets/images/uniform-previews/OUD-42.png";
+import jerseyPreviews43 from "../../assets/images/uniform-previews/OUD-43.png";
+import jerseyPreviews44 from "../../assets/images/uniform-previews/OUD-44.png";
+import jerseyPreviews45 from "../../assets/images/uniform-previews/OUD-45.png";
+import jerseyPreviews46 from "../../assets/images/uniform-previews/OUD-46.png";
 import jerseyPreviews47 from "../../assets/images/uniform-previews/OUD-47.png";
-// import jerseyPreviews48 from "../../assets/images/uniform-previews/OUD-48.png";
-// import jerseyPreviews49 from "../../assets/images/uniform-previews/OUD-49.png";
-// import jerseyPreviews50 from "../../assets/images/uniform-previews/OUD-50.png";
-// import jerseyPreviews51 from "../../assets/images/uniform-previews/OUD-51.png";
-// import jerseyPreviews52 from "../../assets/images/uniform-previews/OUD-52.png";
-// import jerseyPreviews53 from "../../assets/images/uniform-previews/OUD-53.png";
-// import jerseyPreviews54 from "../../assets/images/uniform-previews/OUD-54.png";
-// import jerseyPreviews55 from "../../assets/images/uniform-previews/OUD-55.png";
-// import jerseyPreviews56 from "../../assets/images/uniform-previews/OUD-56.png";
-// import jerseyPreviews57 from "../../assets/images/uniform-previews/OUD-57.png";
-// import jerseyPreviews58 from "../../assets/images/uniform-previews/OUD-58.png";
+import jerseyPreviews48 from "../../assets/images/uniform-previews/OUD-48.png";
+import jerseyPreviews49 from "../../assets/images/uniform-previews/OUD-49.png";
+import jerseyPreviews50 from "../../assets/images/uniform-previews/OUD-50.png";
+import jerseyPreviews51 from "../../assets/images/uniform-previews/OUD-51.png";
+import jerseyPreviews52 from "../../assets/images/uniform-previews/OUD-52.png";
+import jerseyPreviews53 from "../../assets/images/uniform-previews/OUD-53.png";
+import jerseyPreviews54 from "../../assets/images/uniform-previews/OUD-54.png";
+import jerseyPreviews55 from "../../assets/images/uniform-previews/OUD-55.png";
+import jerseyPreviews56 from "../../assets/images/uniform-previews/OUD-56.png";
+import jerseyPreviews57 from "../../assets/images/uniform-previews/OUD-57.png";
+import jerseyPreviews58 from "../../assets/images/uniform-previews/OUD-58.png";
 import jerseyPreviews59 from "../../assets/images/uniform-previews/OUD-59.png";
-// import jerseyPreviews60 from "../../assets/images/uniform-previews/OUD-60.png";
+import jerseyPreviews60 from "../../assets/images/uniform-previews/OUD-60.png";
 import jerseyPreviews61 from "../../assets/images/uniform-previews/OUD-61.png";
-// import jerseyPreviews62 from "../../assets/images/uniform-previews/OUD-62.png";
-// import jerseyPreviews63 from "../../assets/images/uniform-previews/OUD-63.png";
-// import jerseyPreviews64 from "../../assets/images/uniform-previews/OUD-64.png";
-// import jerseyPreviews65 from "../../assets/images/uniform-previews/OUD-65.png";
-// import jerseyPreviews66 from "../../assets/images/uniform-previews/OUD-66.png";
-// import jerseyPreviews67 from "../../assets/images/uniform-previews/OUD-67.png";
-// import jerseyPreviews68 from "../../assets/images/uniform-previews/OUD-68.png";
-// import jerseyPreviews69 from "../../assets/images/uniform-previews/OUD-69.png";
-// import jerseyPreviews70 from "../../assets/images/uniform-previews/OUD-70.png";
-// import jerseyPreviews71 from "../../assets/images/uniform-previews/OUD-71.png";
-// import jerseyPreviews72 from "../../assets/images/uniform-previews/OUD-72.png";
-// import jerseyPreviews73 from "../../assets/images/uniform-previews/OUD-73.png";
-// import jerseyPreviews74 from "../../assets/images/uniform-previews/OUD-74.png";
-// import jerseyPreviews75 from "../../assets/images/uniform-previews/OUD-75.png";
-// import jerseyPreviews76 from "../../assets/images/uniform-previews/OUD-76.png";
-// import jerseyPreviews77 from "../../assets/images/uniform-previews/OUD-77.png";
+import jerseyPreviews62 from "../../assets/images/uniform-previews/OUD-62.png";
+import jerseyPreviews63 from "../../assets/images/uniform-previews/OUD-63.png";
+import jerseyPreviews64 from "../../assets/images/uniform-previews/OUD-64.png";
+import jerseyPreviews65 from "../../assets/images/uniform-previews/OUD-65.png";
+import jerseyPreviews66 from "../../assets/images/uniform-previews/OUD-66.png";
+import jerseyPreviews67 from "../../assets/images/uniform-previews/OUD-67.png";
+import jerseyPreviews68 from "../../assets/images/uniform-previews/OUD-68.png";
+import jerseyPreviews69 from "../../assets/images/uniform-previews/OUD-69.png";
+import jerseyPreviews70 from "../../assets/images/uniform-previews/OUD-70.png";
+import jerseyPreviews71 from "../../assets/images/uniform-previews/OUD-71.png";
+import jerseyPreviews72 from "../../assets/images/uniform-previews/OUD-72.png";
+import jerseyPreviews73 from "../../assets/images/uniform-previews/OUD-73.png";
+import jerseyPreviews74 from "../../assets/images/uniform-previews/OUD-74.png";
+import jerseyPreviews75 from "../../assets/images/uniform-previews/OUD-75.png";
+import jerseyPreviews76 from "../../assets/images/uniform-previews/OUD-76.png";
+import jerseyPreviews77 from "../../assets/images/uniform-previews/OUD-77.png";
 import jerseyPreviews78 from "../../assets/images/uniform-previews/OUD-78.png";
-// import jerseyPreviews79 from "../../assets/images/uniform-previews/OUD-79.png";
-// import jerseyPreviews80 from "../../assets/images/uniform-previews/OUD-80.png";
-// import jerseyPreviews81 from "../../assets/images/uniform-previews/OUD-81.png";
+import jerseyPreviews79 from "../../assets/images/uniform-previews/OUD-79.png";
+import jerseyPreviews80 from "../../assets/images/uniform-previews/OUD-80.png";
+import jerseyPreviews81 from "../../assets/images/uniform-previews/OUD-81.png";
 import jerseyPreviews82 from "../../assets/images/uniform-previews/OUD-82.png";
-// import jerseyPreviews83 from "../../assets/images/uniform-previews/OUD-83.png";
-// import jerseyPreviews84 from "../../assets/images/uniform-previews/OUD-84.png";
-// import jerseyPreviews85 from "../../assets/images/uniform-previews/OUD-85.png";
-// import jerseyPreviews86 from "../../assets/images/uniform-previews/OUD-86.png";
-// import jerseyPreviews87 from "../../assets/images/uniform-previews/OUD-87.png";
+import jerseyPreviews83 from "../../assets/images/uniform-previews/OUD-83.png";
+import jerseyPreviews84 from "../../assets/images/uniform-previews/OUD-84.png";
+import jerseyPreviews85 from "../../assets/images/uniform-previews/OUD-85.png";
+import jerseyPreviews86 from "../../assets/images/uniform-previews/OUD-86.png";
+import jerseyPreviews87 from "../../assets/images/uniform-previews/OUD-87.png";
 import jerseyPreviews88 from "../../assets/images/uniform-previews/OUD-88.png";
 import jerseyPreviews89 from "../../assets/images/uniform-previews/OUD-89.png";
 
@@ -124,58 +124,58 @@ const jerseyPreviewsList = [
   { id: 33, src: jerseyPreviews33, alt: "Preview 33" },
   { id: 34, src: jerseyPreviews34, alt: "Preview 34" },
   { id: 35, src: jerseyPreviews35, alt: "Preview 35" },
-  //   { id: 36, src: jerseyPreviews36, alt: "Preview 36" },
-  //   { id: 37, src: jerseyPreviews37, alt: "Preview 37" },
+  { id: 36, src: jerseyPreviews36, alt: "Preview 36" },
+  { id: 37, src: jerseyPreviews37, alt: "Preview 37" },
   { id: 38, src: jerseyPreviews38, alt: "Preview 38" },
-  //   { id: 39, src: jerseyPreviews39, alt: "Preview 39" },
-  //   { id: 40, src: jerseyPreviews40, alt: "Preview 40" },
-  //   { id: 41, src: jerseyPreviews41, alt: "Preview 41" },
-  //   { id: 42, src: jerseyPreviews42, alt: "Preview 42" },
-  //   { id: 43, src: jerseyPreviews43, alt: "Preview 43" },
-  //   { id: 44, src: jerseyPreviews44, alt: "Preview 44" },
-  //   { id: 45, src: jerseyPreviews45, alt: "Preview 45" },
-  //   { id: 46, src: jerseyPreviews46, alt: "Preview 46" },
+  { id: 39, src: jerseyPreviews39, alt: "Preview 39" },
+  { id: 40, src: jerseyPreviews40, alt: "Preview 40" },
+  { id: 41, src: jerseyPreviews41, alt: "Preview 41" },
+  { id: 42, src: jerseyPreviews42, alt: "Preview 42" },
+  { id: 43, src: jerseyPreviews43, alt: "Preview 43" },
+  { id: 44, src: jerseyPreviews44, alt: "Preview 44" },
+  { id: 45, src: jerseyPreviews45, alt: "Preview 45" },
+  { id: 46, src: jerseyPreviews46, alt: "Preview 46" },
   { id: 47, src: jerseyPreviews47, alt: "Preview 47" },
-  //   { id: 48, src: jerseyPreviews48, alt: "Preview 48" },
-  //   { id: 49, src: jerseyPreviews49, alt: "Preview 49" },
-  //   { id: 50, src: jerseyPreviews50, alt: "Preview 50" },
-  //   { id: 51, src: jerseyPreviews51, alt: "Preview 51" },
-  //   { id: 52, src: jerseyPreviews52, alt: "Preview 52" },
-  //   { id: 53, src: jerseyPreviews53, alt: "Preview 53" },
-  //   { id: 54, src: jerseyPreviews54, alt: "Preview 54" },
-  //   { id: 55, src: jerseyPreviews55, alt: "Preview 55" },
-  //   { id: 56, src: jerseyPreviews56, alt: "Preview 56" },
-  //   { id: 57, src: jerseyPreviews57, alt: "Preview 57" },
-  //   { id: 58, src: jerseyPreviews58, alt: "Preview 58" },
+  { id: 48, src: jerseyPreviews48, alt: "Preview 48" },
+  { id: 49, src: jerseyPreviews49, alt: "Preview 49" },
+  { id: 50, src: jerseyPreviews50, alt: "Preview 50" },
+  { id: 51, src: jerseyPreviews51, alt: "Preview 51" },
+  { id: 52, src: jerseyPreviews52, alt: "Preview 52" },
+  { id: 53, src: jerseyPreviews53, alt: "Preview 53" },
+  { id: 54, src: jerseyPreviews54, alt: "Preview 54" },
+  { id: 55, src: jerseyPreviews55, alt: "Preview 55" },
+  { id: 56, src: jerseyPreviews56, alt: "Preview 56" },
+  { id: 57, src: jerseyPreviews57, alt: "Preview 57" },
+  { id: 58, src: jerseyPreviews58, alt: "Preview 58" },
   { id: 59, src: jerseyPreviews59, alt: "Preview 59" },
-  //   { id: 60, src: jerseyPreviews60, alt: "Preview 60" },
+  { id: 60, src: jerseyPreviews60, alt: "Preview 60" },
   { id: 61, src: jerseyPreviews61, alt: "Preview 61" },
-  //   { id: 62, src: jerseyPreviews62, alt: "Preview 62" },
-  //   { id: 63, src: jerseyPreviews63, alt: "Preview 63" },
-  //   { id: 64, src: jerseyPreviews64, alt: "Preview 64" },
-  //   { id: 65, src: jerseyPreviews65, alt: "Preview 65" },
-  //   { id: 66, src: jerseyPreviews66, alt: "Preview 66" },
-  //   { id: 67, src: jerseyPreviews67, alt: "Preview 67" },
-  //   { id: 68, src: jerseyPreviews68, alt: "Preview 68" },
-  //   { id: 69, src: jerseyPreviews69, alt: "Preview 69" },
-  //   { id: 70, src: jerseyPreviews70, alt: "Preview 70" },
-  //   { id: 71, src: jerseyPreviews71, alt: "Preview 71" },
-  //   { id: 72, src: jerseyPreviews72, alt: "Preview 72" },
-  //   { id: 73, src: jerseyPreviews73, alt: "Preview 73" },
-  //   { id: 74, src: jerseyPreviews74, alt: "Preview 74" },
-  //   { id: 75, src: jerseyPreviews75, alt: "Preview 75" },
-  //   { id: 76, src: jerseyPreviews76, alt: "Preview 76" },
-  //   { id: 77, src: jerseyPreviews77, alt: "Preview 77" },
+  { id: 62, src: jerseyPreviews62, alt: "Preview 62" },
+  { id: 63, src: jerseyPreviews63, alt: "Preview 63" },
+  { id: 64, src: jerseyPreviews64, alt: "Preview 64" },
+  { id: 65, src: jerseyPreviews65, alt: "Preview 65" },
+  { id: 66, src: jerseyPreviews66, alt: "Preview 66" },
+  { id: 67, src: jerseyPreviews67, alt: "Preview 67" },
+  { id: 68, src: jerseyPreviews68, alt: "Preview 68" },
+  { id: 69, src: jerseyPreviews69, alt: "Preview 69" },
+  { id: 70, src: jerseyPreviews70, alt: "Preview 70" },
+  { id: 71, src: jerseyPreviews71, alt: "Preview 71" },
+  { id: 72, src: jerseyPreviews72, alt: "Preview 72" },
+  { id: 73, src: jerseyPreviews73, alt: "Preview 73" },
+  { id: 74, src: jerseyPreviews74, alt: "Preview 74" },
+  { id: 75, src: jerseyPreviews75, alt: "Preview 75" },
+  { id: 76, src: jerseyPreviews76, alt: "Preview 76" },
+  { id: 77, src: jerseyPreviews77, alt: "Preview 77" },
   { id: 78, src: jerseyPreviews78, alt: "Preview 78" },
-  //   { id: 79, src: jerseyPreviews79, alt: "Preview 79" },
-  //   { id: 80, src: jerseyPreviews80, alt: "Preview 80" },
-  //   { id: 81, src: jerseyPreviews81, alt: "Preview 81" },
+  { id: 79, src: jerseyPreviews79, alt: "Preview 79" },
+  { id: 80, src: jerseyPreviews80, alt: "Preview 80" },
+  { id: 81, src: jerseyPreviews81, alt: "Preview 81" },
   { id: 82, src: jerseyPreviews82, alt: "Preview 82" },
-  //   { id: 83, src: jerseyPreviews83, alt: "Preview 83" },
-  //   { id: 84, src: jerseyPreviews84, alt: "Preview 84" },
-  //   { id: 85, src: jerseyPreviews85, alt: "Preview 85" },
-  //   { id: 86, src: jerseyPreviews86, alt: "Preview 86" },
-  //   { id: 87, src: jerseyPreviews87, alt: "Preview 87" },
+  { id: 83, src: jerseyPreviews83, alt: "Preview 83" },
+  { id: 84, src: jerseyPreviews84, alt: "Preview 84" },
+  { id: 85, src: jerseyPreviews85, alt: "Preview 85" },
+  { id: 86, src: jerseyPreviews86, alt: "Preview 86" },
+  { id: 87, src: jerseyPreviews87, alt: "Preview 87" },
   { id: 88, src: jerseyPreviews88, alt: "Preview 88" },
   { id: 89, src: jerseyPreviews89, alt: "Preview 89" },
 ];
